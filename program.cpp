648d93637d4f55222b9cd46a00c8f457a89b0f7b
DazedGodzilla <nene.muse@gmail.com>
2019-04-16 16:43:46 +0200

    Initial commit

---
+++ program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printMyName () {
+	cout<< "Renata Vukadinovic" << endl;
+}
+
+void printMyIndex () {
+	cout << "142013"<<endl;
+}
+
+int main () {
+	printMyName();
+	printMyIndex();
+}
